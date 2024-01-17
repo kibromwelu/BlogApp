0440a248e116004efd5d1ceac7a7be226f0c8b11
kibromwelu <56187902+kibromwelu@users.noreply.github.com>
2024-01-17 21:38:19 +0300

    Update

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="blog-home"),
     path("about/", views.about, name="blog-about"),
+    path("detail/<int:id>", views.detail, name="detail"),
 ]
